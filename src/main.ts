3f76c874790c9f75ce525f86605584f88c3447be
youen <youen.valun@gmail.com>
2026-01-20 12:18:22 +0100

    init project with nest - prisma - clerk - resend

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  });
+
+  // validation des DTO
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+void bootstrap();
